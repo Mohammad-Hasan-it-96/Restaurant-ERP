830f8e81d39ed7dec7d4dac05e2d2da8bbd23f6e
Mohamad Hasan <mohamad.hassan@ramaaz.com>
2026-05-26 15:33:31 +0300

    comment CategorySeeder and ProductSeeder from DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,8 +26,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SystemConfigSeeder::class,
             LanguageSeeder::class,
-            CategorySeeder::class,
-            ProductSeeder::class,
+//            CategorySeeder::class,
+//            ProductSeeder::class,
             DeliveryZoneSeeder::class,
         ]);
     }
